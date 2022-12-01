01db02d8995653f702ddd6560d6dcd4f06f3c30b
Chad Fusco <fusco@case.edu>
2022-12-01 11:40:22 -0800

    installed morgan middleware, added morgan to package.json

--- server/index.js
+++ server/index.js
@@ -1,35 +1,38 @@
 // let's...get...stAH-ted
 
+// REQUIRE STATEMENTS
 require("dotenv").config();
-
 const express = require("express");
 const path = require("path");
+const axios = require('axios');
 
 const app = express();
 
-// App-wide middleware
+// APP-WIDE MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// Static service of assets
+// STATIC SERVICE OF ASSETS
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-// Routes
-// app.get('/songs', controllers.getSongs);
-// app.post('/songs', controllers.addSong);
-// app.get('/songs/:_id', controllers.getSong);
+// ROUTES
+app.all('/*', (req, res) => {
+  axios({
+    method: req.method,
+    url: path.join(process.env.API_URL, req.url), // req.url will include query (?) and params (:)
+    headers: {
+      'User-Agent': 'request', // this might not be necessary?
+      'Authorization': process.env.AUTH
+    }
+  })
+  .then(resAPI => res.status(resAPI.status).send(resAPI.data))
+  .catch(errAPI => {
+    // console.log('errAPI:', errAPI)
+    res.status(errAPI.response.status).send(errAPI);
+  })
+});
 
+// PORT AND SERVER LISTEN
 const PORT = process.env.PORT || 3300;
-
 app.listen(PORT);
 console.log(`Server listening at http://localhost:${PORT}`);
-
-
-// *********
-// axios.get(Window.server + '/songs')
-// .then(res => {
-//   setSongs(res.data);
-// })
-// .catch(err => {
-//   console.log('ERROR IN AXIOS.GET:', err)
-// })
