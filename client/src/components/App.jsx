330c0d9902e78bf7dcff0cab05a591717e1419c7
Thomas Saldana <saldana.tom@gmail.com>
2022-12-02 19:31:09 -0800

    Setup client side axios request and inital app states

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import requests from '../requests.js';
 import Overview from './Overview.jsx';
 import RelatedOutfit from './RelatedOutfit/Index.jsx';
 import RatingsReviews from './RatingsReviews/Index.jsx';
 
 const App = () => {
-  //hooks
+  const [products, setProducts] = useState([]);
+  const [current, setCurrent] = useState({});
+  const [metadata, setMetadata] = useState([]);
 
-  //functions
+  useEffect(() => {
+    requests.getProducts((data) => {
+      setProducts(data);
+      setCurrent(data[0]);
+      requests.getMetadata(data[0].id, (data) => {
+        setMetadata(data);
+      });
+    });
+  }, []);
+
+  // console.log(products);
+  // console.log(current);
+  // console.log(metadata);
 
   return (
     <div>
