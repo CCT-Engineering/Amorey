bdca24e532dc9f9834efd3fdbb8a0ecd19211a56
Thomas Saldana <saldana.tom@gmail.com>
2022-12-03 19:54:01 -0800

    calculate star percentages

--- client/src/components/RatingsReviews/RatingBreakdown.jsx
+++ client/src/components/RatingsReviews/RatingBreakdown.jsx
@@ -1,21 +1,10 @@
 import React from 'react';
 
-const renderReviewGraph = (starCount) => {
-  return starCount;
-};
-
-const calculateAverageStars = (ratings) => {
-  let totalStars = 0
-  let ratingsCount = 0;
-
-  for (const key in ratings) {
-    totalStars += key * ratings[key];
-    ratingsCount += Number(ratings[key]);
-  }
-  const average = totalStars / ratingsCount;
-  //formula for star chart filled in to nearest quarter star
-  // Math.round(average * 4) / 4).toFixed(2)
-  return (Math.round(average * 4) / 4).toFixed(1);
+const renderReviewGraph = (ratings, starCount) => {
+  const totalStars = Object.keys(ratings).reduce((previous, key) => {
+    return previous + Number(ratings[key])
+  }, 0)
+  return `${ratings[starCount]} out of ${totalStars}`;
 };
 
 const calculateRecommenedPercent = (recommend) => {
@@ -28,13 +17,13 @@ const RatingBreakdown = ({ratings, recommend, stars}) => {
 
   return (
     <div>
-      <h2>{calculateAverageStars(ratings)} *display {stars}*</h2>
+      <h2>{(Math.round(stars * 4) / 4).toFixed(1)} *display {stars}*</h2>
       <span>{calculateRecommenedPercent(recommend)}% of reviews recommend this product</span>
-      <div>5 stars {renderReviewGraph(ratings[5])}</div>
-      <div>4 stars {renderReviewGraph(ratings[4])}</div>
-      <div>3 stars {renderReviewGraph(ratings[3])}</div>
-      <div>2 stars {renderReviewGraph(ratings[2])}</div>
-      <div>1 stars {renderReviewGraph(ratings[1])}</div>
+      <div>5 stars {renderReviewGraph(ratings, 5)}</div>
+      <div>4 stars {renderReviewGraph(ratings, 4)}</div>
+      <div>3 stars {renderReviewGraph(ratings, 3)}</div>
+      <div>2 stars {renderReviewGraph(ratings, 2)}</div>
+      <div>1 stars {renderReviewGraph(ratings, 1)}</div>
     </div>
   );
 };
