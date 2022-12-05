a2844567fdbc0499955c053b6e05ad6b4ed7c994
Thomas Saldana <saldana.tom@gmail.com>
2022-12-05 13:19:16 -0800

    setup basic bar chart for star ratings

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
     'no-restricted-globals': 'off',
     // allow arrow functions
     'arrow-body-style': 'off',
-    // allow react compnent as return for function
+    // allow react component as return for function
     'react/function-component-definition': 'off',
     // related to button click on non-button html element
     'jsx-a11y/no-static-element-interactions': 'off',
--- client/src/components/RatingsReviews/RatingBreakdown.jsx
+++ client/src/components/RatingsReviews/RatingBreakdown.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import local from '../../styles/RatingsReviews.css';
 
+const handleClick = (index) => {
+  event.preventDefault();
+  // console.log('here: ', index);
+};
+
 const reviewGraph = (ratings, starCount) => {
   const totalStars = Object.keys(ratings).reduce((total, key) => {
     return total + Number(ratings[key]);
   }, 0);
-  return `${ratings[starCount]} out of ${totalStars}`;
+  const percent = (ratings[starCount] / totalStars).toFixed(2) * 100;
+
+  return (
+    <div className={local.barDimensions}>
+      <div className={local.barProgress} style={{ width: `${percent}%` }} />
+    </div>
+  );
 };
 
 const recommenedPercent = (recommend) => {
   const approve = Number(recommend.true);
   const reject = Number(recommend.false);
   return Math.floor((approve / (approve + reject)) * 100);
-};
-
-const handleClick = (index) => {
-  event.preventDefault();
-  // console.log('here: ', index);
 };
 
 const RatingBreakdown = ({ ratings, recommend, stars }) => {
@@ -36,37 +42,32 @@ const RatingBreakdown = ({ ratings, recommend, stars }) => {
         {recommenedPercent(recommend)}
         % of reviews recommend this product
       </div>
-      <div className={local.starChart}>
-        <span className={local.starListing}>
-          5 stars
-        </span>
-        <a className={local.starGraph} onClick={() => handleClick(4)}>
+      <div className={local.starTable}>
+        <div className={local.graph}>
+          5
+          <p className={local.graphText}> Stars</p>
           {reviewGraph(ratings, 5)}
-        </a>
-      </div>
-      <div className={local.starChart}>
-        <span className={local.starListing}>4 stars</span>
-        <a className={local.starGraph} onClick={() => handleClick(3)}>
+        </div>
+        <div className={local.graph}>
+          4
+          <p className={local.graphText}> Stars</p>
           {reviewGraph(ratings, 4)}
-        </a>
-      </div>
-      <div className={local.starChart}>
-        <span className={local.starListing}>3 stars</span>
-        <a className={local.starGraph} onClick={() => handleClick(2)}>
+        </div>
+        <div className={local.graph}>
+          3
+          <p className={local.graphText}> Stars</p>
           {reviewGraph(ratings, 3)}
-        </a>
-      </div>
-      <div className={local.starChart}>
-        <span className={local.starListing}>2 stars</span>
-        <a className={local.starGraph} onClick={() => handleClick(1)}>
+        </div>
+        <div className={local.graph}>
+          2
+          <p className={local.graphText}> Stars</p>
           {reviewGraph(ratings, 2)}
-        </a>
-      </div>
-      <div className={local.starChart}>
-        <span className={local.starListing}>1 stars</span>
-        <a className={local.starGraph} onClick={() => handleClick(0)}>
+        </div>
+        <div className={local.graph}>
+          1
+          <p className={local.graphText}> Stars</p>
           {reviewGraph(ratings, 1)}
-        </a>
+        </div>
       </div>
     </div>
   );
