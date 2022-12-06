3ffd9238de7362a45ebfae256e53d1a15dd60e48
Thomas Saldana <saldana.tom@gmail.com>
2022-12-06 14:22:42 -0800

    setup React Test Library dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: path.join(__dirname, "client/src", "index.js"),
+  mode: 'development',
+  entry: path.join(__dirname, 'client/src', 'index.js'),
   output: {
-    path: path.resolve(__dirname, "client/dist"),
+    path: path.resolve(__dirname, 'client/dist'),
   },
   module: {
     rules: [
@@ -13,30 +13,30 @@ module.exports = {
         test: /\.(jsx|js)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
       {
         test: /\.css$/,
         use: [
-          "style-loader",
+          'style-loader',
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               importLoaders: 1,
               modules: true,
             },
           },
         ],
-      }
-    ]
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "client/src", "index.html"),
+      template: path.join(__dirname, 'client/src', 'index.html'),
     }),
   ],
-}
+};
