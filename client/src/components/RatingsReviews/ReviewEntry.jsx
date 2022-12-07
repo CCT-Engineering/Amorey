df49b9ab221e64493101d4d60785cfdf1105a77d
Thomas Saldana <saldana.tom@gmail.com>
2022-12-06 21:31:01 -0800

    added util file to format date

--- client/src/components/RatingsReviews/ReviewEntry.jsx
+++ client/src/components/RatingsReviews/ReviewEntry.jsx
@@ -1,39 +1,7 @@
 import React, { useState } from 'react';
 import { buildHandleEnterKeyPress } from '../../util';
 import local from '../../styles/RatingsReviews/ReviewEntry.css';
-
-const formatDate = (date) => {
-  const year = date.substring(0, 4);
-  const month = date.substring(5, 7);
-  const day = date[8] === '0' ? date.substring(9, 10) : date.substring(8, 10);
-  let string = '';
-  if (month === '01') {
-    string = `January ${day}, ${year}`;
-  } else if (month === '02') {
-    string = `Feburary ${day}, ${year}`;
-  } else if (month === '03') {
-    string = `March ${day}, ${year}`;
-  } else if (month === '04') {
-    string = `April ${day}, ${year}`;
-  } else if (month === '05') {
-    string = `May ${day}, ${year}`;
-  } else if (month === '06') {
-    string = `June ${day}, ${year}`;
-  } else if (month === '07') {
-    string = `July ${day}, ${year}`;
-  } else if (month === '08') {
-    string = `August ${day}, ${year}`;
-  } else if (month === '09') {
-    string = `September ${day}, ${year}`;
-  } else if (month === '10') {
-    string = `October ${day}, ${year}`;
-  } else if (month === '11') {
-    string = `November ${day}, ${year}`;
-  } else {
-    string = `December ${day}, ${year}`;
-  }
-  return string;
-};
+import date from '../../util/formatDate.js';
 
 const ReviewEntry = ({ review, update }) => {
   const [expand, setExpand] = useState(false);
@@ -52,7 +20,7 @@ const ReviewEntry = ({ review, update }) => {
     <div className={local.main}>
       <div className={local.header}>
         <div className={local.rating}>{`${review.rating}★★★★★`}</div>
-        <div className={local.user}>{`${review.reviewer_name}, ${formatDate(review.date)}`}</div>
+        <div className={local.user}>{`${review.reviewer_name}, ${date(review.date)}`}</div>
       </div>
       <h4 className={local.summary}>{review.summary}</h4>
       {review.body.length < 250 || expand
