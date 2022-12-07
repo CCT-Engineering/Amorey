19a33c168ab850fb2c153e9ca626e3130e36afcb
Thomas Saldana <saldana.tom@gmail.com>
2022-12-06 18:38:55 -0800

    Added Ratings and Reviews test

---
+++ src/tests/RatingsReviews.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import RatingsReviews from '../../client/src/components/RatingsReviews/Index.jsx';
+
+describe('Render App Page', () => {
+  it('Should render page with Title header', () => {
+    render(<RatingsReviews />);
+
+    const title = screen.getByText('RATINGS & REVIEWS');
+    expect(title).toHaveTextContent('RATINGS & REVIEWS');
+
+    const heading = screen.getByRole('heading', { level: 5 });
+    expect(heading).toBeInTheDocument();
+  });
+});
