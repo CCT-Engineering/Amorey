eb9e84dcc10d91348a72e320ec0e18c2df7872eb
Chris Wong <wongcpie@gmail.com>
2022-12-08 11:26:19 -1000

    PR to update

--- client/src/components/RelatedOutfit/Index.jsx
+++ client/src/components/RelatedOutfit/Index.jsx
@@ -3,7 +3,12 @@ import local from '../../styles/RelatedOutfit.css';
 import RelatedList from './RelatedList.jsx';
 import OutfitList from './OutfitList.jsx';
 
-const Index = ({ current, setCurrent, setFavorites, favorites, CurMeta }) => {
+const Index = ({ current, setCurrent, setFavorites, favorites, CurMeta, currentStyles }) => {
+//   console.log('cur styles', currentStyles)
+// console.log('cur styles 0', currentStyles[0])
+// console.log('cur styles photos', currentStyles[0].photos)
+// console.log('cur styles photos 0', currentStyles[0].photos[0])
+// console.log('cur styles photos 0', currentStyles[0].photos[0].thumbnail_url)
   return (
     <div className={local.RelatedOutfit}>
       <h4>RELATED PRODUCTS</h4>
