194d15e56c6a1070cacff2b17486f14f2dbae555
Chad Fusco <fusco@case.edu>
2022-12-07 23:24:43 -0800

    eslint-plugin-jest-dom

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'plugin:testing-library/react',
   ],
   overrides: [
   ],
@@ -15,6 +16,7 @@ module.exports = {
   },
   plugins: [
     'react',
+    'testing-library',
   ],
   rules: {
     // allow .js .jsx for file imports
@@ -34,5 +36,11 @@ module.exports = {
 
     // related to button click on non-button html element
     'jsx-a11y/anchor-is-valid': 'off',
+
+    // REACT TESTING LIBRARY LINTER RULES: https://github.com/testing-library/eslint-plugin-testing-library
+    'testing-library/await-async-query': 'error',
+    'testing-library/no-await-sync-query': 'error',
+    'testing-library/no-debugging-utils': 'warn',
+    'testing-library/no-dom-import': 'off',
   },
 };
