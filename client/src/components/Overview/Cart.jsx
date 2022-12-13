a25b56f509ebb1bfac0b415ebdd95bf359b8a2c2
Chad Fusco <fusco@case.edu>
2022-12-13 12:53:02 -0800

    progress on add cart

--- client/src/components/Overview/Cart.jsx
+++ client/src/components/Overview/Cart.jsx
@@ -4,6 +4,13 @@ import local from '../../styles/Overview/Cart.css';
 function Cart() {
   return (
     <div className={local.cart}>
+      <div className={local.sizeQty}>
+        <select className={"darkMode" ? local.sizeQtyMenu : local.sizeQtyMenu} role="menu" id="Size">
+          <option value="relevant">relevance</option>
+          <option value="newest">newest</option>
+          <option value="helpful">helpful</option>
+        </select>
+      </div>
       <p>This will be the cart</p>
     </div>
   );
