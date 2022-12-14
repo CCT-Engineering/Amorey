50c29014fe25103acaf81adcfd0cd9bc473dcd6b
Chris Wong <wongcpie@gmail.com>
2022-12-13 17:34:33 -1000

    added logo

--- client/src/components/RelatedOutfit/OutfitList.jsx
+++ client/src/components/RelatedOutfit/OutfitList.jsx
@@ -69,10 +69,6 @@ const OutfitList = ({
     <div className={local.carousel}>
       <div className={local.outfit}>
         <button type="button" onClick={addOutfit} className={darkMode ? local.addCardDark : local.addCard}>+ Add Current To Outfit</button>
-        {/* <div onClick={addOutfit} className={local.addCard}>
-          <div>+</div>
-          <div>Add Current To Outfit</div>
-        </div> */}
         <div
           role="button"
           tabIndex={0}
