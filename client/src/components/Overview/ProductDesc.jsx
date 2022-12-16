0ce147a2d559ff7b1f4b16694211ea2b0b745639
Chad Fusco <fusco@case.edu>
2022-12-16 12:48:25 -0800

    saved social media icons to assests folder, switch divider line in Product Description to light color in dark mode

--- client/src/components/Overview/ProductDesc.jsx
+++ client/src/components/Overview/ProductDesc.jsx
@@ -3,16 +3,28 @@ import local from '../../styles/Overview/ProductDesc.css';
 import FeatureEntry from './FeatureEntry.jsx';
 
 function ProductDesc({ current, darkMode }) {
+  const productDescBackground = darkMode
+    ? { background: '#1E556C' }
+    : { background: 'rgb(172, 233, 222)' };
+
+  const dividerColor = darkMode
+    ? { background: '#B6D8C4' }
+    : { background: '#555' };
+
   return (
     <div
-      className={darkMode ? local.productDescDark : local.productDesc}
+      className={local.productDesc}
+      style={productDescBackground}
     >
       <div className={local.sloganDesc}>
         <h4>{current.slogan}</h4>
         <p className={local.desc}>{current.description}</p>
       </div>
       <div className={local.dividerContainer}>
-        <div className={local.divider} />
+        <div
+          className={local.divider}
+          style={dividerColor}
+        />
       </div>
       <div className={local.features}>
         {current.features.map((feat) => (<FeatureEntry key={feat.feature} feat={feat} />))}
