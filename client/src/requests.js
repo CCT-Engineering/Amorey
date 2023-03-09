c48ea1a051b5366bc99a2df4c78e967797ca4923
Chad Fusco <fusco@case.edu>
2023-03-08 17:14:13 -0800

    converted all methods in requests.js from Axios to fetch

--- client/src/requests.js
+++ client/src/requests.js
@@ -2,70 +2,73 @@ import axios from 'axios';
 
 const url = window.location.href.includes('amazonaws') ? window.location.href : 'http://localhost:3300/';
 
+// getData helper function for GET requests
+const getData = (res, callback) => (
+  res.json()
+    .then((data) => callback(data))
+    .catch((err) => console.error(err))
+);
+
 const requests = {
   // pass in (callback)
   getProducts: (callback) => {
     fetch(`${url}products/?page=1&count=1`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('data from fetch:', data);
-        callback(data);
-      })
-      .catch((err) => console.error(err));
+      .then((res) => getData(res, callback));
   },
 
   // pass in (product_id, callback)
   getProductInfo: (id, callback) => {
-    axios.get(`${url}products/${id}`)
-      .then(({ data }) => callback(data))
-      .catch((err) => console.error(err));
+    fetch(`${url}products/${id}`)
+      .then((res) => getData(res, callback));
   },
 
   // pass in (product_id, callback)
   getStyles: (id, callback) => {
-    axios.get(`${url}products/${id}/styles`)
-      .then(({ data }) => callback(data))
-      .catch((err) => console.error(err));
+    fetch(`${url}products/${id}/styles`)
+      .then((res) => getData(res, callback));
   },
 
   // pass in (product_id, callback)
   getRelated: (id, callback) => {
-    axios.get(`${url}products/${id}/related`)
-      .then(({ data }) => callback(data))
-      .catch((err) => console.error(err));
+    fetch(`${url}products/${id}/related`)
+      .then((res) => getData(res, callback));
   },
 
   // pass in (product_id, sort method, callback)
   getReviews: (id, sort, callback) => {
-    axios.get(`${url}reviews/?product_id=${id}&sort=${sort}&count=999`)
-      .then(({ data }) => callback(data))
-      .catch((err) => console.error(err));
+    fetch(`${url}reviews/?product_id=${id}&sort=${sort}&count=999`)
+      .then((res) => getData(res, callback));
   },
 
   // pass in (product_id)
   getMetadata: (id, callback) => {
-    axios.get(`${url}reviews/meta/?product_id=${id}`)
-      .then(({ data }) => callback(data))
-      .catch((err) => console.error(err));
+    fetch(`${url}reviews/meta/?product_id=${id}`)
+      .then((res) => getData(res, callback));
   },
 
   // pass in (review_id)
   putHelpful: (id, callback) => {
-    axios.put(`${url}reviews/${id}/helpful`)
+    fetch(`${url}reviews/${id}/helpful`, { method: 'PUT' })
       .then(() => callback())
       .catch((err) => console.error(err));
   },
 
   // pass in (review_id)
   putReport: (id, callback) => {
-    axios.put(`${url}reviews/${id}/report`)
+    fetch(`${url}reviews/${id}/report`, { method: 'PUT' })
       .then(() => callback())
       .catch((err) => console.error(err));
   },
 
   // pass in (new review object)
   postReview: (review, callback) => {
-    axios.post(`${url}reviews`, review)
+    console.log('review:', review);
+    const options = {
+      method: 'POST',
+      headers: new Headers({ 'content-type': 'application/json' }),
+      body: JSON.stringify(review),
+    };
+    fetch(`${url}reviews`, options)
       .then(() => callback())
       .catch((err) => console.error(err));
   },
