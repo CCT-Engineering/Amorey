a9b292ee13573a9280a791248ab370e13b431277
Chad Fusco <fusco@case.edu>
2024-04-28 17:41:47 -0700

    WIP on Skeleton Loading effect for Gallery. Remaining work: integrate with changing photos in expanded view

--- client/src/components/Overview/Gallery.jsx
+++ client/src/components/Overview/Gallery.jsx
@@ -2,6 +2,7 @@ import React, {
   useState, useRef, createRef, useEffect,
 } from 'react';
 import local from '../../styles/Overview/Gallery.css';
+import global from '../../styles/global.css';
 import Thumb from './Thumb.jsx';
 import Social from './Social.jsx';
 import { buildHandleEnterKeyPress, buildHandleKeyDown, formatImg } from '../../util';
@@ -18,14 +19,21 @@ function Gallery({
   // TOP_OFFSET is photo offset from top of window.
   const TOP_OFFSET = 100;
   const ZOOM = 2.5;
+  const lowerResImage = useRef(new Image());
+  const highResImage = useRef(new Image());
 
   const photoDescPrefix = 'Main photo';
   const photoDesc = `${photoDescPrefix} ${photoIndex} of ${name} style`;
   const photoQty = photos.reduce((acc, photo) => acc + (photo.url ? 1 : 0), 0);
 
   const [expandView, setExpandView] = useState(false);
+  const expandViewRef = useRef(expandView);
+  useEffect(() => {
+    expandViewRef.current = expandView;
+  }, [expandView]);
+
   const [zoomView, setZoomView] = useState(false);
-  const photoUrl = photos[photoIndex] ? photos[photoIndex].url : '';
+  const [mainPhotoClass, setMainPhotoClass] = useState(`${local.galleryLoading} ${global.skeleton}`);
   const [mainPhotoStyle, setMainPhotoStyle] = useState({});
 
   // States and Ref below are for expanded view zoom feature
@@ -35,35 +43,73 @@ function Gallery({
   const thumbRefs = useRef([]);
   thumbRefs.current = photos.map((_, i) => thumbRefs.current[i] ?? createRef());
 
-  const preloadHigherResImage = (highResPhotoIdx = photoIndex) => {
-    const highResPhotoUrl = photos[highResPhotoIdx].url;
-    const preloadImage = new Image();
-    preloadImage.src = formatImg(highResPhotoUrl, null, windowHgt * 1.5, false);
+  const preloadHigherResImage = (imgObj, highResPhotoIdx = photoIndex) => {
+    const photoUrl = photos[highResPhotoIdx].url;
+    // eslint-disable-next-line no-param-reassign
+    imgObj.src = formatImg(photoUrl, null, windowHgt * 1.5, false);
+  };
+
+  const changeMainPhotoClassToGallery = (isViewExpanded = false) => {
+    const imgObj = (isViewExpanded ? highResImage : lowerResImage).current;
+    console.log('\timgObj:', imgObj);
+    if (imgObj.src && imgObj.complete) { // main product photo is loaded
+      console.log('PHOTO LOADED - SET CLASS TO GALLERY');
+      const newAttr = { backgroundImage: `url(${imgObj.src})` };
+      setMainPhotoStyle((prevStyle) => ({ ...prevStyle, ...newAttr }));
+      setMainPhotoClass(isViewExpanded ? local.galleryExp : local.gallery);
+      return true; // means image is loaded
+    }
+    console.log('PHOTO NOT LOADED - SET CLASS TO SKELETON');
+    setMainPhotoStyle({});
+    setMainPhotoClass(`${local[isViewExpanded ? 'galleryExpLoading' : 'galleryLoading']} ${global.skeleton}`);
+    return false; // means image still loading
   };
 
   useEffect(() => {
+    // this callback is only for initializing and preloading images,
+    // and setting gallery class/styles when photoIndex changes in non-expanded view.
+    lowerResImage.current = new Image();
+    highResImage.current = new Image();
+    changeMainPhotoClassToGallery(expandView);
     // preload the lowerResImage to create a trigger to preload the higher res
     // once the lowerResImage is finished loading.
-    if (photoUrl) {
-      const lowerResImage = new Image();
-      lowerResImage.src = formatImg(photoUrl, MAIN_PHOTO_WID, MAIN_PHOTO_HGT);
-      lowerResImage.onload = () => {
-        if (expandView) {
-          if (photoIndex + 1 < photos.length) {
-            preloadHigherResImage(photoIndex + 1);
-            setTimeout(() => preloadHigherResImage(), 800);
-          }
-          if (photoIndex > 0) {
-            preloadHigherResImage(photoIndex - 1);
-          }
-        } else {
-          setTimeout(() => preloadHigherResImage(), 800);
-        }
-      };
-    }
-  }, [photoUrl, windowHgt]);
+    if (photos[photoIndex]?.url) {
+      console.log('LOADING LOW RES IMG');
+      lowerResImage.current.src = formatImg(photos[photoIndex].url, MAIN_PHOTO_WID, MAIN_PHOTO_HGT);
 
-  const handleBtnClick = (e) => {
+      lowerResImage.current.onload = () => {
+        setTimeout(() => {
+          console.log('LOW RES MAIN IMG LOADED');
+          console.log('expandView:', expandViewRef.current);
+          if (expandViewRef.current) {
+            // switched images within expanded view or just opened expanded view
+            // if just opened, handleMainImgClick handles gallery styling
+            // if switching, handleArrowBtnClick handles gallery styling
+            console.log('IN EXPANDED VIEW');
+            preloadHigherResImage(highResImage.current);
+            if (photoIndex + 1 < photos.length) {
+              preloadHigherResImage(new Image(), photoIndex + 1);
+            }
+            if (photoIndex > 0) {
+              preloadHigherResImage(new Image(), photoIndex - 1);
+            }
+          } else {
+            // NOT in expanded view
+            changeMainPhotoClassToGallery(false);
+            preloadHigherResImage(highResImage.current);
+          }
+        }, 4000);
+      };
+    } else {
+      setMainPhotoStyle({
+        background: 'whitesmoke',
+        border: '1px solid #111',
+        cursor: 'not-allowed',
+      });
+    }
+  }, [photos, photoIndex, windowHgt]);
+
+  const handleArrowBtnClick = (e) => {
     e.preventDefault();
     e.stopPropagation();
     const decrButtons = ['left', 'thumbUp'];
@@ -77,16 +123,16 @@ function Gallery({
     });
   };
 
-  function buildBtn(className, btnName, onKeyDownBindings, content) {
+  function buildArrowBtn(className, btnName, onKeyDownBindings, content) {
     const newBtn = (
       <button
         type="button"
         tabIndex={0}
         className={className}
         name={btnName}
-        onClick={handleBtnClick}
-        onKeyPress={buildHandleEnterKeyPress(handleBtnClick)}
-        onKeyDown={buildHandleKeyDown(handleBtnClick, onKeyDownBindings)}
+        onClick={handleArrowBtnClick}
+        onKeyPress={buildHandleEnterKeyPress(handleArrowBtnClick)}
+        onKeyDown={buildHandleKeyDown(handleArrowBtnClick, onKeyDownBindings)}
       >
         {content}
       </button>
@@ -95,6 +141,7 @@ function Gallery({
   }
 
   const handleMainImgClick = (e) => {
+    console.log('invoked handleMainImgClick');
     e.preventDefault();
     let newAttr;
     if (expandView) {
@@ -120,16 +167,14 @@ function Gallery({
     } else {
       // if NOT in Expanded view
       setExpandView(true);
-      newAttr = {
-        backgroundImage: `url(${formatImg(photoUrl, null, windowHgt * 1.5, false)})`,
-      };
-      if (photoIndex + 1 < photos.length) {
-        preloadHigherResImage(photoIndex + 1);
+
+      if (!changeMainPhotoClassToGallery(true)) {
+        console.log('CHECKPOINT');
+        highResImage.current.onload = () => {
+          console.log('~~~~~~~~~~~~~~~~~~~HIGH RES IMG LOADED');
+          changeMainPhotoClassToGallery(true);
+        };
       }
-      if (photoIndex > 0) {
-        preloadHigherResImage(photoIndex - 1);
-      }
-      setMainPhotoStyle((prevStyle) => ({ ...prevStyle, ...newAttr }));
     }
   };
 
@@ -137,12 +182,13 @@ function Gallery({
     e.preventDefault();
     e.stopPropagation();
     setExpandView(false);
+    changeMainPhotoClassToGallery();
     const newAttr = {
       transform: '',
       cursor: '',
       marginTop: '',
       marginRight: '',
-      backgroundImage: `url(${formatImg(photoUrl, MAIN_PHOTO_WID, MAIN_PHOTO_HGT)})`,
+      backgroundImage: `url(${lowerResImage.current.src})`,
     };
     setMainPhotoStyle((prevStyle) => ({ ...prevStyle, ...newAttr }));
   };
@@ -187,28 +233,10 @@ function Gallery({
     setMainPhotoStyle((prevStyle) => ({ ...prevStyle, ...newAttr }));
   }, [offset]);
 
-  useEffect(() => {
-    if (!photoUrl) {
-      setMainPhotoStyle({
-        background: 'whitesmoke',
-        border: '1px solid #111',
-        cursor: 'not-allowed',
-      });
-    } else if (expandView) {
-      setMainPhotoStyle({
-        backgroundImage: `url(${formatImg(photoUrl, null, windowHgt * 1.5, false)})`,
-      });
-    } else {
-      setMainPhotoStyle({
-        backgroundImage: `url(${formatImg(photoUrl, MAIN_PHOTO_WID, MAIN_PHOTO_HGT)})`,
-      });
-    }
-  }, [photoUrl]);
-
-  const gallerySide = !photoUrl ? '' : (
+  const gallerySide = !photos[photoIndex]?.url ? '' : (
     <div className={local.gallerySide}>
       {photoIndex > 0
-        ? buildBtn(local.thbArrow, 'thumbUp', ['ArrowUp'], '˄')
+        ? buildArrowBtn(local.thbArrow, 'thumbUp', ['ArrowUp'], '˄')
         : <button type="button" tabIndex={0} className={local.thbArrow}>-</button>}
       <div className={local.galleryThumbs}>
         {photos.map((photo, i) => (
@@ -224,7 +252,7 @@ function Gallery({
         ))}
       </div>
       {photoIndex < photoQty - 1
-        ? buildBtn(local.thbArrow, 'thumbDn', ['ArrowDown'], '˅')
+        ? buildArrowBtn(local.thbArrow, 'thumbDn', ['ArrowDown'], '˅')
         : <button type="button" tabIndex={0} className={local.thbArrow}>-</button>}
     </div>
   );
@@ -234,16 +262,16 @@ function Gallery({
       name="mainImg"
       role="button"
       tabIndex={0}
-      className={expandView ? local.galleryExp : local.gallery}
+      className={mainPhotoClass}
       style={mainPhotoStyle}
       aria-label={photoDesc}
       onClick={photoQty ? handleMainImgClick : () => {}}
       onKeyPress={photoQty ? buildHandleEnterKeyPress(handleMainImgClick) : () => {}}
       onMouseMove={handleMouseMove}
     >
-      {photoUrl ? gallerySide : <p className={local.noPhoto}>Photo Unavailable</p>}
-      {photoIndex === 0 ? '' : buildBtn(local.left, 'left', ['ArrowLeft'], '⮕')}
-      {photoIndex === photoQty - 1 ? '' : buildBtn(local.right, 'right', ['ArrowRight'], '⮕')}
+      {photos[photoIndex]?.url ? gallerySide : <p className={local.noPhoto}>Photo Unavailable</p>}
+      {photoIndex === 0 ? '' : buildArrowBtn(local.left, 'left', ['ArrowLeft'], '⮕')}
+      {photoIndex === photoQty - 1 ? '' : buildArrowBtn(local.right, 'right', ['ArrowRight'], '⮕')}
       {expandView
         ? (
           <button
